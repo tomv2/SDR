51ba0c92593cda1dc456a68155b04fcc2913c084
tomv2 <89455845+tomv2@users.noreply.github.com>
2023-02-20 18:31:22 +0000

    Update testingfile.py

--- testingfile.py
+++ testingfile.py
@@ -1,30 +1,16 @@
-import matplotlib.pyplot as plt
-import matplotlib.pyplot as plts
 from rtlsdr import *
+from pylab import *
 
 sdr = RtlSdr()
 
-ppm = sdr.get_freq_correction()
-print 'Frequency correction ' + str(ppm)
-tuner_type = sdr.get_tuner_type()
-print 'Tuner type ' + str(tuner_type)
+sdr.sample_rate = 3.2e6
+sdr.center_freq = 95e6
+sdr.gain = 5
 
-sdr.sample_rate = 2.4e6
-sdr.center_freq = 392.0125e6
-sdr.gain = 7
+samples = sdr.read_samples(500e3)
 
-plts.title('Power spectral density')
-plts.xlabel('Frequency (MHz)')
-plts.ylabel('Relative power (dB)')
-plts.grid(True)
-lines, = plts.plot(0, 0)
-plts.ion()
-fig = plts.figure()
+psd(samples, NFFT=1024, Fs=sdr.rs/1e6, Fc=sdr.fc/1e6)   
+xlabel('Frequency (MHz)')
+ylabel('Relative power (dB)')
 
-while True:
-    samples = sdr.read_samples(256*1024)
-    plt.psd(samples, NFFT=512, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
-    line = plt.gca().lines[0]
-    lines.set_xdata(line.get_xdata())
-    lines.set_ydata(line.get_ydata())
-    fig.canvas.draw()
+show()
