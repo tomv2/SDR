56c8105b42e9ba4e9d4dc10c95046ff44c458be1
tomv2 <89455845+tomv2@users.noreply.github.com>
2023-02-21 20:15:05 +0000

    Update testingfile.py

--- testingfile.py
+++ testingfile.py
@@ -7,7 +7,7 @@ sdr.sample_rate = 3.2e6
 sdr.center_freq = 100e6
 sdr.gain = 5
 
-samples = sdr.read_samples(500e3)
+samples = sdr.read_samples(256*1024)
 
 psd(samples, NFFT=1024, Fs=sdr.rs/1e6, Fc=sdr.fc/1e6)   
 xlabel('Frequency (MHz)')
