ea0e44ecdfb2299f8bae89490ecd2256cc5da51d
tomv2 <89455845+tomv2@users.noreply.github.com>
2023-02-21 20:33:02 +0000

    Update testingfile.py

--- testingfile.py
+++ testingfile.py
@@ -1,16 +1,13 @@
 from rtlsdr import *
 from pylab import *
-import numpy as np
 
 sdr = RtlSdr()
 
 sdr.sample_rate = 3.2e6
-sdr.center_freq = 100e6
+sdr.center_freq = 95e6
 sdr.gain = 5
 
-samples = sdr.read_samples(256*1024)
-
-x = 0.5 * np.angle(samples[0:-1] * np.conj(samples[1:]))
+samples = sdr.read_samples(500e3)
 
 psd(samples, NFFT=1024, Fs=sdr.rs/1e6, Fc=sdr.fc/1e6)   
 xlabel('Frequency (MHz)')
