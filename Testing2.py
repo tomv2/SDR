56441cd29fdcbeeeeab0174d853236f3c09e26ed
tomv2 <89455845+tomv2@users.noreply.github.com>
2023-04-17 09:18:34 +0100

    Update Testing2.py

--- Testing2.py
+++ Testing2.py
@@ -1,28 +1,43 @@
 from rtlsdr import RtlSdr
 import matplotlib.pyplot as plt
 import numpy as np
+from pylab import *
 
-sdr = RtlSdr()
 
-sdr.sample_rate = 2.4e6
-sdr.freq_correction = 60
-sdr.gain = 'auto'
+def update_psd():
+    # create SDR instance
+    sdr = RtlSdr()
 
-plt.ion()  # Turn on interactive mode
-fig, ax = plt.subplots()
-lines = ax.plot([], [])
-ax.set_xlabel('Freq')
-ax.set_ylabel('dB')
-ax.set_title('Spectrum')
+    # configure device
+    sdr.sample_rate = 2.4e6
+    sdr.center_freq = 95e6
+    sdr.gain = 4
 
-while True:
-    for i in range(90, 110, 2):
-        sdr.center_freq = i*1e6
-        samples = sdr.read_samples(256*1024)
-        freqs, psd = plt.psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
-        lines[0].set_data(freqs, psd)
-        ax.relim()
-        ax.autoscale_view()
-        fig.canvas.draw()
-        fig.canvas.flush_events()
+    while True:
+        try:
+            # read samples
+            samples = sdr.read_samples(256*1024)
+
+            # plot PSD
+            psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
+            xlabel('Frequency (MHz)')
+            ylabel('Relative power (dB)')
+            draw()
+
+        except KeyboardInterrupt:
+            break
+
+    # close SDR device
+    sdr.close()
+
+# initialize plot
+ion()
+figure()
+
+# start updating PSD
+update_psd()
+
+# block until plot window is closed
+ioff()
+show()
 
