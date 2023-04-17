7c3492da350a1f5c234767ddb1c13452c6689eb8
tomv2 <89455845+tomv2@users.noreply.github.com>
2023-04-17 10:31:34 +0100

    Update Testing2.py

--- Testing2.py
+++ Testing2.py
@@ -1,43 +1,44 @@
-from rtlsdr import RtlSdr
-import matplotlib.pyplot as plt
 import numpy as np
-from pylab import *
+import matplotlib.pyplot as plt
+import time
+from rtlsdr import RtlSdr
 
+# configure SDR
+sdr = RtlSdr()
+sdr.sample_rate = 2.4e6
+sdr.center_freq = 140e6
+sdr.gain = 'auto'
 
-def update_psd():
-    # create SDR instance
-    sdr = RtlSdr()
+Fs = sdr.sample_rate
+Ts = 1/Fs # sample period
+N = 256*1024
+noise_power = 2
 
-    # configure device
-    sdr.sample_rate = 2.4e6
-    sdr.center_freq = 95e6
-    sdr.gain = 4
+# create the figure and axis for the plot
+fig, ax = plt.subplots()
+ax.set_xlabel("Frequency [Hz]")
+ax.set_ylabel("Magnitude [dB]")
 
-    while True:
-        try:
-            # read samples
-            samples = sdr.read_samples(256*1024)
+ax.grid(True)
 
-            # plot PSD
-            psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
-            xlabel('Frequency (MHz)')
-            ylabel('Relative power (dB)')
-            draw()
+while True:
+    samples = sdr.read_samples(N)
+    PSD = np.abs(np.fft.fft(samples))**2 / (N*Fs)
+    PSD_log = 10.0*np.log10(PSD)
+    PSD_shifted = np.fft.fftshift(PSD_log)
+    print(PSD_shifted)
 
-        except KeyboardInterrupt:
-            break
+    f = np.arange(-Fs/2, Fs/2, Fs/N) # start, stop, step
 
-    # close SDR device
-    sdr.close()
-
-# initialize plot
-ion()
-figure()
-
-# start updating PSD
-update_psd()
-
-# block until plot window is closed
-ioff()
-show()
-
+    # update the data for the plot
+    ax.clear()
+    ax.plot(f, PSD_shifted)
+    ax.set_xlabel("Frequency [Hz]")
+    ax.set_ylabel("Magnitude [dB]")
+    # ax.set_xlim(-Fs/2, Fs/2)
+    # ax.set_ylim(-60, 60)
+    ax.grid(True)
+    plt.pause(0.01) # pause briefly to show the plot
+    
+# close the SDR
+sdr.close()
