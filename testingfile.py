39bbe0f2fd065798ac213ae7f393e3455c661632
tomv2 <89455845+tomv2@users.noreply.github.com>
2023-04-17 20:47:47 +0100

    Update testingfile.py

--- testingfile.py
+++ testingfile.py
@@ -1,54 +1,47 @@
 import numpy as np
-from scipy import signal
+import matplotlib.pyplot as plt
+from scipy.signal import decimate, firwin, resample_poly
 from rtlsdr import RtlSdr
 
-# Set RTL-SDR parameters
-freq = 100e6
+# Setup the RTL-SDR device
 sdr = RtlSdr()
-sdr.sample_rate = 2.4e6
-sdr.center_freq = freq
-sdr.gain = 20
 
-# Collect samples
+# Set the sample rate and center frequency
+sample_rate = 2.4e6
+center_freq = 100e6
+sdr.sample_rate = sample_rate
+sdr.center_freq = center_freq
+
+# Read samples from the RTL-SDR
 samples = sdr.read_samples(256*1024)
 
 # Freq shift
 N = len(samples)
-f_o = -57e3 # amount we need to shift by
-t = np.arange(N)/sdr.sample_rate # time vector
+f_o = -1e6 # amount we need to shift by
+t = np.arange(N)/sample_rate # time vector
 samples = samples * np.exp(2j*np.pi*f_o*t) # down shift
 
 # Low-Pass Filter
-taps = signal.firwin(numtaps=101, cutoff=7500, fs=sdr.sample_rate)
+taps = firwin(numtaps=101, cutoff=0.5e6, fs=sample_rate)
 samples = np.convolve(samples, taps, 'valid')
 
 # Decimate by 10, now that we filtered and there won't be aliasing
-samples = samples[::10]
-sdr.sample_rate /= 10
+samples = decimate(samples, 10)
 
-# Resample to 19kHz
-samples = signal.resample_poly(samples, 19*sdr.sample_rate, sdr.sample_rate)
-sdr.sample_rate = 19*sdr.sample_rate
+# Resample to 19 kHz
+samples = resample_poly(samples, 19e3, sample_rate)
 
-# Perform quadrature demodulation
-I = np.real(samples)
-Q = np.imag(samples)
-samples = I[:-1]*Q[1:] - I[1:]*Q[:-1]
+# Compute the spectrum
+frequencies, spectrum = plt.psd(samples, NFFT=1024, Fs=19e3, scale_by_freq=True, window=np.blackman(1024))
 
-# Perform symbol synchronization
-samples = samples[1500:-1500]
-samples = samples-np.mean(samples)
-samples = np.sign(samples)
+# Convert the spectrum to dBm
+spectrum_dbm = 10 * np.log10(spectrum) + 30
 
-# Calculate spectrum
-f, Pxx = signal.welch(x=samples, fs=sdr.sample_rate, nperseg=1024)
-
-# Convert power to dBm
-Pxx = 10*np.log10(Pxx/1e-3)
-
-# Plot spectrum
-import matplotlib.pyplot as plt
-plt.plot(f/1e6, Pxx)
+# Plot the spectrum
+plt.plot(frequencies/1e6, spectrum_dbm)
 plt.xlabel('Frequency (MHz)')
 plt.ylabel('Power Spectral Density (dBm/Hz)')
 plt.show()
+
+# Close the RTL-SDR device
+sdr.close()
