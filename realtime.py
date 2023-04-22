8795a8c7a2ad4dde191d8abf55a525f259fb6528
tomv2 <89455845+tomv2@users.noreply.github.com>
2023-04-22 11:32:57 +0100

    Update realtime.py

--- realtime.py
+++ realtime.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import rtlsdr
-from collections import deque
+import threading
 
 # Set up the SDR device
 sdr = rtlsdr.RtlSdr()
@@ -19,34 +19,39 @@ ax.set_ylabel('Power (dB)')
 
 # Set up the circular buffer
 buffer_size = 1024
-sample_buffer = deque(maxlen=buffer_size)
+sample_buffer = np.zeros(buffer_size, dtype=np.complex64)
+buffer_lock = threading.Lock()
+
+# Define a function to read samples from the SDR device and update the buffer
+def read_samples():
+    global sample_buffer
+    while True:
+        with buffer_lock:
+            samples = sdr.read_samples(buffer_size)
+            sample_buffer[:-buffer_size] = sample_buffer[buffer_size:]
+            sample_buffer[-buffer_size:] = samples
 
 # Define a function to update the plot
 def update_plot():
-    # Concatenate the most recent samples from the buffer
-    samples = np.concatenate(list(sample_buffer))
-    
-    # Compute the power spectrum density
-    PSD = np.abs(np.fft.fft(samples))**2 / (len(samples)*sdr.sample_rate)
-    PSD_log = 10*np.log10(PSD)
-    PSD_shifted = np.fft.fftshift(PSD_log)
-    
-    # Compute the frequency axis
-    freq_axis = np.fft.fftfreq(len(samples), 1/sdr.sample_rate)
-    freq_axis = np.fft.fftshift(freq_axis)
-    freq_axis = freq_axis + sdr.center_freq
-    
-    # Update the plot with the new data
-    line.set_data(freq_axis/1e6, PSD_shifted)
-    ax.relim()
-    ax.autoscale_view()
-    fig.canvas.draw()
+    while True:
+        with buffer_lock:
+            samples = sample_buffer.copy()
+        PSD = np.abs(np.fft.fft(samples))**2 / (len(samples)*sdr.sample_rate)
+        PSD_log = 10*np.log10(PSD)
+        PSD_shifted = np.fft.fftshift(PSD_log)
+        freq_axis = np.fft.fftfreq(len(samples), 1/sdr.sample_rate)
+        freq_axis = np.fft.fftshift(freq_axis)
+        freq_axis = freq_axis + sdr.center_freq
+        line.set_data(freq_axis/1e6, PSD_shifted)
+        ax.relim()
+        ax.autoscale_view()
+        fig.canvas.draw()
 
 # Start the SDR device and the real-time plot
-for i in range(1000):
-    samples = sdr.read_samples(buffer_size)
-    sample_buffer.append(samples)
-    update_plot()
+t1 = threading.Thread(target=read_samples)
+t2 = threading.Thread(target=update_plot)
+t1.start()
+t2.start()
 
 plt.show()
 
