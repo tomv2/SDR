797488d105b8e8b00b76f16ea645fc8e0fbb413f
tomv2 <89455845+tomv2@users.noreply.github.com>
2023-04-22 11:19:41 +0100

    Update realtime.py

--- realtime.py
+++ realtime.py
@@ -1,28 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pyrtlsdr
+import rtlsdr
+from collections import deque
 
-sdr = pyrtlsdr.RtlSdr()
-
-# Set the sample rate, center frequency, and gain
+# Set up the SDR device
+sdr = rtlsdr.RtlSdr()
 sdr.sample_rate = 3.2e6
 sdr.center_freq = 100e6
 sdr.gain = 'auto'
 
-# Create a figure for the real-time plot
+# Set up the real-time plot
 fig, ax = plt.subplots()
-
-# Create an empty line object for the plot
 line, = ax.plot([], [])
-
-# Set the plot limits and labels
 ax.set_xlim(sdr.center_freq/1e6 - 1, sdr.center_freq/1e6 + 1)
 ax.set_ylim(-120, 20)
 ax.set_xlabel('Frequency (MHz)')
 ax.set_ylabel('Power (dB)')
 
-# Define a function to update the plot with new samples
-def update_plot(samples):
+# Set up the circular buffer
+buffer_size = 1024
+sample_buffer = deque(maxlen=buffer_size)
+
+# Define a function to update the plot
+def update_plot():
+    # Concatenate the most recent samples from the buffer
+    samples = np.concatenate(list(sample_buffer))
+    
     # Compute the power spectrum density
     PSD = np.abs(np.fft.fft(samples))**2 / (len(samples)*sdr.sample_rate)
     PSD_log = 10*np.log10(PSD)
@@ -40,7 +43,10 @@ def update_plot(samples):
     fig.canvas.draw()
 
 # Start the SDR device and the real-time plot
-sdr.read_samples_async(update_plot, num_samples=8192)
+for i in range(1000):
+    samples = sdr.read_samples(buffer_size)
+    sample_buffer.append(samples)
+    update_plot()
 
 plt.show()
 
