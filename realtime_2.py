a29556ef5dba713d507581c71bf7321c0eb6379e
tomv2 <89455845+tomv2@users.noreply.github.com>
2023-04-22 11:50:59 +0100

    Update realtime_2.py

--- realtime_2.py
+++ realtime_2.py
@@ -1,59 +1,76 @@
-import threading
-import time
 import numpy as np
 import matplotlib.pyplot as plt
 import rtlsdr
+import threading
+import queue
 import usb.core
 import usb.util
 
-class SDRThread(threading.Thread):
-    def __init__(self):
-        super().__init__()
+class SDR:
+    def __init__(self, center_frequency, sample_rate):
+        self.center_frequency = center_frequency
+        self.sample_rate = sample_rate
         self.sdr = rtlsdr.RtlSdr()
-        self.Fs = 3.2e6
-        self.Ts = 1/self.Fs
-        self.N = 256*1024
-        self.center_frequency = 100e6
-        self.sdr.sample_rate = self.Fs
+
+        self.sdr.sample_rate = self.sample_rate
         self.sdr.center_frequency = self.center_frequency
 
-    def run(self):
+    def get_samples(self, N):
         try:
-            while True:
-                samples = self.sdr.read_samples(self.N)
-                PSD = np.abs(np.fft.fft(samples))**2 / (self.N*self.Fs)
+            samples = self.sdr.read_samples(N)
+            return samples
+        except usb.core.USBError:
+            usb.util.dispose_resources(self.sdr.dev)
+            self.sdr = rtlsdr.RtlSdr()
+            self.sdr.sample_rate = self.sample_rate
+            self.sdr.center_frequency = self.center_frequency
+            return None
+
+class Plotter:
+    def __init__(self, queue):
+        self.queue = queue
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_xlabel("Frequency Hz")
+        self.ax.set_ylabel("Power dB")
+        self.ax.grid(True)
+
+    def start(self):
+        self.thread = threading.Thread(target=self._plot_thread)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def _plot_thread(self):
+        while True:
+            try:
+                samples, freq_axis = self.queue.get()
+                PSD = np.abs(np.fft.fft(samples))**2 / (len(samples)*Fs)
                 PSD_log = 10*np.log10(PSD)
                 PSD_shifted = np.fft.fftshift(PSD_log)
-
-                freq_axis = np.fft.fftfreq(len(samples), 1/self.Fs)
-                freq_axis = np.fft.fftshift(freq_axis)
-                freq_axis = freq_axis + self.center_frequency
                 f = freq_axis/1e6
 
-                plt.clf()  # clear the previous plot
-                plt.plot(f, PSD_shifted)
-                plt.xlabel("Frequency Hz")
-                plt.ylabel("Power dB")
-                plt.grid(True)
-                plt.pause(0.01)  # pause to allow plot to update
+                self.ax.clear()
+                self.ax.plot(f, PSD_shifted)
+                plt.pause(0.01)
 
-        except usb.core.USBError as e:
-            print(f"USB error: {e}")
+            except Exception as e:
+                print(e)
 
-        finally:
-            self.sdr.close()
+if __name__ == '__main__':
+    Fs = 3.2e6
+    Ts = 1/Fs
+    N = 256 * 1024
+    center_frequency = 100e6
 
-if __name__ == "__main__":
-    try:
-        thread = SDRThread()
-        thread.start()
+    sdr = SDR(center_frequency, Fs)
+    plot_queue = queue.Queue()
+    plot = Plotter(plot_queue)
+    plot.start()
 
-        while True:
-            time.sleep(1)
+    while True:
+        samples = sdr.get_samples(N)
+        if samples is not None:
+            freq_axis = np.fft.fftfreq(len(samples), Ts)
+            freq_axis = np.fft.fftshift(freq_axis)
+            freq_axis = freq_axis + center_frequency
 
-    except KeyboardInterrupt:
-        pass
-
-    finally:
-        thread.sdr.cancel_read_async()
-        thread.sdr.close()
+            plot_queue.put((samples, freq_axis))
