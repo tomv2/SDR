5eef6a49a50b3e1fa746acf5b2b7333c4f49307a
tomv2 <89455845+tomv2@users.noreply.github.com>
2023-04-26 11:01:46 +0100

    Update waterfall.py

--- waterfall.py
+++ waterfall.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib
+matplotlib.use('Qt5Agg')  # set backend to Qt5
 import matplotlib.pyplot as plt
 import rtlsdr
 import threading
@@ -8,6 +10,13 @@ sdr = rtlsdr.RtlSdr()
 sdr.sample_rate = 3.2e6
 sdr.center_freq = 100e6
 sdr.gain = 'auto'
+
+# Set up the real-time plot
+fig, ax = plt.subplots()
+line = ax.imshow(np.zeros((512, 1024)), aspect='auto', cmap='viridis')
+ax.set_xlabel('Time')
+ax.set_ylabel('Frequency (MHz)')
+fig.colorbar(line)
 
 # Set up the circular buffer
 buffer_size = 1024
@@ -25,45 +34,22 @@ def read_samples():
 
 # Define a function to update the plot
 def update_plot():
-    fig, ax = plt.subplots()
-    plt.ion()
-    fig.show()
-    fig.canvas.draw()
-    
-    PSD_log_all = []
-    freq_axis = None
     while True:
         with buffer_lock:
             samples = sample_buffer.copy()
-        PSD = np.abs(np.fft.fft(samples))**2 / (len(samples)*sdr.sample_rate)
+        PSD = np.abs(np.fft.fft(samples, axis=0))**2 / (len(samples)*sdr.sample_rate)
         PSD_log = 10*np.log10(PSD)
-        PSD_log_all.append(PSD_log)
-        
-        # Update the frequency axis once
-        if freq_axis is None:
-            freq_axis = np.fft.fftfreq(len(samples), 1/sdr.sample_rate)
-            freq_axis = np.fft.fftshift(freq_axis)
-            freq_axis = freq_axis + sdr.center_freq
-            freq_axis = np.resize(freq_axis, (len(samples)//buffer_size, buffer_size))
-            freq_axis = np.fft.fftshift(freq_axis, axes=1)
-            extent = [freq_axis.min()/1e6, freq_axis.max()/1e6, 0, len(PSD_log_all)]
-
-        PSD_log_all_resized = np.array(PSD_log_all)
-        if PSD_log_all_resized.shape[0] > len(samples)//buffer_size:
-            PSD_log_all_resized = PSD_log_all_resized[-(len(samples)//buffer_size):, :]
-
-        ax.imshow(PSD_log_all_resized.T, origin='lower', aspect='auto', cmap='jet', extent=extent)
-        ax.set_xlabel('Frequency (MHz)')
-        ax.set_ylabel('FFT Window')
+        PSD_shifted = np.fft.fftshift(PSD_log, axes=0)
+        freq_axis = np.fft.fftfreq(len(samples), 1/sdr.sample_rate)
+        freq_axis = np.fft.fftshift(freq_axis) + sdr.center_freq/1e6
+        line.set_data(np.arange(PSD_shifted.shape[1]), freq_axis, PSD_shifted)
         fig.canvas.draw()
-        fig.canvas.flush_events()
 
 # Start the SDR device and the real-time plot
-sdr_thread = threading.Thread(target=read_samples)
-sdr_thread.start()
-
-plot_thread = threading.Thread(target=update_plot)
-plot_thread.start()
+t1 = threading.Thread(target=read_samples)
+t2 = threading.Thread(target=update_plot)
+t1.start()
+t2.start()
 
 plt.show()
 
