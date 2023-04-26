e2a678fb500dde2bf35bcbc2e56fdb5ef7602ae6
tomv2 <89455845+tomv2@users.noreply.github.com>
2023-04-26 13:27:47 +0100

    Update waterfall.py

--- waterfall.py
+++ waterfall.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import rtlsdr
 import threading
+import os
 
 # Set up the SDR device
 sdr = rtlsdr.RtlSdr()
@@ -9,12 +10,12 @@ sdr.sample_rate = 3.2e6
 sdr.center_freq = 100e6
 sdr.gain = 'auto'
 
-# Set up the waterfall plot
-waterfall = np.zeros((1000, 1024))
+# Set up the real-time plot
 fig, ax = plt.subplots()
-im = ax.imshow(waterfall, cmap='jet', aspect='auto', extent=[sdr.center_freq/1e6 - 1, sdr.center_freq/1e6 + 1, 0, 1000*sdr.sample_rate/1024/1e3])
+waterfall = np.zeros((1000, 1024))
+im = ax.imshow(waterfall, cmap='jet', aspect='auto')
 ax.set_xlabel('Frequency (MHz)')
-ax.set_ylabel('Time (ms)')
+ax.set_ylabel('Time (100 ms)')
 
 # Set up the circular buffer
 buffer_size = 1024
@@ -30,8 +31,8 @@ def read_samples():
             sample_buffer[:-buffer_size] = sample_buffer[buffer_size:]
             sample_buffer[-buffer_size:] = samples
 
-# Define a function to update the waterfall plot
-def update_waterfall():
+# Define a function to update the plot
+def update_plot():
     global waterfall
     while True:
         with buffer_lock:
@@ -39,19 +40,22 @@ def update_waterfall():
         PSD = np.abs(np.fft.fft(samples))**2 / (len(samples)*sdr.sample_rate)
         PSD_log = 10*np.log10(PSD)
         PSD_shifted = np.fft.fftshift(PSD_log)
-        freq_axis = np.fft.fftfreq(len(samples), 1/sdr.sample_rate)
-        freq_axis = np.fft.fftshift(freq_axis)
-        freq_axis = freq_axis + sdr.center_freq
-        waterfall[:-1, :] = waterfall[1:, :]
-        waterfall[-1, :] = PSD_shifted
+        waterfall[:-1] = waterfall[1:]
+        waterfall[-1] = PSD_shifted
         im.set_data(waterfall)
+        fig.canvas.draw()
         plt.pause(0.001)
 
-# Start the SDR device and the waterfall plot
+# Start the SDR device and the real-time plot
+os.set_blocking(0, False)
+sdr.read_samples(256)
+os.set_blocking(0, True)
+
 t1 = threading.Thread(target=read_samples)
-t2 = threading.Thread(target=update_waterfall)
-t1.start()
+t2 = threading.Thread(target=update_plot)
+
 t2.start()
+t1.start()
 
 plt.show()
 
