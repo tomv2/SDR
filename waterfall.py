33121b1327b129ff3f8c27690cc2bb7f46d751a0
tomv2 <89455845+tomv2@users.noreply.github.com>
2023-04-26 13:48:20 +0100

    Update waterfall.py

--- waterfall.py
+++ waterfall.py
@@ -1,63 +1,80 @@
+from __future__ import division
+import matplotlib.animation as animation
+from matplotlib.mlab import psd
+import pylab as pyl
 import numpy as np
-import matplotlib.pyplot as plt
-import rtlsdr
-import threading
-import os
+import sys
+from rtlsdr import RtlSdr
 
-# Set up the SDR device
-sdr = rtlsdr.RtlSdr()
-sdr.sample_rate = 3.2e6
-sdr.center_freq = 100e6
-sdr.gain = 'auto'
+# A simple waterfall, spectrum plotter
+#
+# Controls:
+#
+# * Scroll mouse-wheel up or down, or press the left or right arrow keys, to
+#   change the center frequency (hold shift for finer control).
+# * Press "+" and "-" to control gain, and space to enable AGC.
+# * Type a frequency (in MHz) and press enter to directly change the center frequency
 
-# Set up the real-time plot
-fig, ax = plt.subplots()
-waterfall = np.zeros((1000, 1024))
-im = ax.imshow(waterfall, cmap='jet', aspect='auto')
-ax.set_xlabel('Frequency (MHz)')
-ax.set_ylabel('Time (100 ms)')
+NFFT = 1024*4
+NUM_SAMPLES_PER_SCAN = NFFT*16
+NUM_BUFFERED_SWEEPS = 100
 
-# Set up the circular buffer
-buffer_size = 1024
-sample_buffer = np.zeros(buffer_size, dtype=np.complex64)
-buffer_lock = threading.Lock()
+# change this to control the number of scans that are combined in a single sweep
+# (e.g. 2, 3, 4, etc.) Note that it can slow things down
+NUM_SCANS_PER_SWEEP = 1
 
-# Define a function to read samples from the SDR device and update the buffer
-def read_samples():
-    global sample_buffer
-    while True:
-        with buffer_lock:
-            samples = sdr.read_samples(buffer_size)
-            sample_buffer[:-buffer_size] = sample_buffer[buffer_size:]
-            sample_buffer[-buffer_size:] = samples
+# these are the increments when scrolling the mouse wheel or pressing '+' or '-'
+FREQ_INC_COARSE = 1e6
+FREQ_INC_FINE = 0.1e6
+GAIN_INC = 5
 
-# Define a function to update the plot
-def update_plot():
-    global waterfall
-    while True:
-        with buffer_lock:
-            samples = sample_buffer.copy()
-        PSD = np.abs(np.fft.fft(samples))**2 / (len(samples)*sdr.sample_rate)
-        PSD_log = 10*np.log10(PSD)
-        PSD_shifted = np.fft.fftshift(PSD_log)
-        waterfall[:-1] = waterfall[1:]
-        waterfall[-1] = PSD_shifted
-        im.set_data(waterfall)
-        fig.canvas.draw()
-        plt.pause(0.001)
+class Waterfall(object):
+    keyboard_buffer = []
+    shift_key_down = False
+    image_buffer = -100*np.ones((NUM_BUFFERED_SWEEPS,\
+                                 NUM_SCANS_PER_SWEEP*NFFT))
 
-# Start the SDR device and the real-time plot
-os.set_blocking(0, False)
-sdr.read_samples(256)
-os.set_blocking(0, True)
+    def __init__(self, sdr=None, fig=None):
+        self.fig = fig if fig else pyl.figure()
+        self.sdr = sdr if sdr else RtlSdr()
 
-t1 = threading.Thread(target=read_samples)
-t2 = threading.Thread(target=update_plot)
+        self.init_plot()
 
-t2.start()
-t1.start()
+    def init_plot(self):
+        self.ax = self.fig.add_subplot(1,1,1)
+        self.image = self.ax.imshow(self.image_buffer, aspect='auto',\
+                                    interpolation='nearest', vmin=-50, vmax=10)
+        self.ax.set_xlabel('Current frequency (MHz)')
+        self.ax.get_yaxis().set_visible(False)
+        self.colorbar = self.set_colorbar()
 
-plt.show()
+        self.fig.canvas.mpl_connect('scroll_event', self.on_scroll)
+        self.fig.canvas.mpl_connect('key_press_event', self.on_key_press)
+        self.fig.canvas.mpl_connect('key_release_event', self.on_key_release)
 
-# Close the SDR device when the plot is closed
-sdr.close()
+    def set_colorbar(self):
+        """Sets a color bar for the plot."""
+        colorbar = self.fig.colorbar(self.image)
+        colorbar.set_label('Power (dB)')
+        return colorbar
+
+    def set_center_freq(self, freq):
+        """Sets center frequency based on user input."""
+        center_freq = float(freq)*1e6
+        self.sdr.fc = center_freq
+        self.update_plot_labels()
+
+    def update_plot_labels(self):
+        fc = self.sdr.fc
+        rs = self.sdr.rs
+        freq_range = (fc - rs/2)/1e6, (fc + rs*(NUM_SCANS_PER_SWEEP - 0.5))/1e6
+
+        self.image.set_extent(freq_range + (0, 1))
+        self.fig.canvas.draw_idle()
+
+    def on_scroll(self, event):
+        if event.button == 'up':
+            self.sdr.fc += FREQ_INC_FINE if self.shift_key_down else FREQ_INC_COARSE
+            self.update_plot_labels()
+        elif event.button == 'down':
+            self.sdr.fc -= FREQ_INC_FINE if self.shift_key_down else F
